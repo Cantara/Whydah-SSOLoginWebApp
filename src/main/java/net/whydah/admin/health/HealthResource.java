320622b876668bf324196a2034b2ee82dbd64d58
Thor Henning Hetland <totto@totto.org>
2015-12-14 17:44:17 +0100

    lets see...

--- src/main/java/net/whydah/admin/health/HealthResource.java
+++ src/main/java/net/whydah/admin/health/HealthResource.java
@@ -2,30 +2,22 @@ package net.whydah.admin.health;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
  * Endpoint for health check, copied from UIB.
  */
 @Controller
-@Path("/health")
 public class HealthResource {
     private static final Logger log = LoggerFactory.getLogger(HealthResource.class);
 
     public HealthResource() {
     }
 
-    @GET
-    @RequestMapping("/login")
-    @Produces(MediaType.TEXT_PLAIN)
+    @RequestMapping("/health")
     public Response isHealthy() {
         boolean ok = true;
         log.trace("isHealthy={}", ok);
