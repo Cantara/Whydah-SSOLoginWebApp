12cfb8a239fdc86a6d2bf019d599ba55ec1041e8
Thor Henning Hetland <totto@totto.org>
2015-12-14 17:53:21 +0100

    getting there :)

--- src/main/java/net/whydah/admin/health/HealthResource.java
+++ src/main/java/net/whydah/admin/health/HealthResource.java
@@ -1,10 +1,14 @@
 package net.whydah.admin.health;
 
+import net.whydah.sso.authentication.whydah.SessionHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.Response;
 
 /**
@@ -18,13 +22,15 @@ public class HealthResource {
     }
 
     @RequestMapping("/health")
-    public Response isHealthy() {
+    public Response isHealthy(HttpServletRequest request, HttpServletResponse response, Model model) {
         boolean ok = true;
         log.trace("isHealthy={}", ok);
         if (ok) {
+            model.addAttribute("health", "Status OK!");
             return Response.ok("Status OK!").build();
         } else {
             //Intentionally not returning anything the client can use to determine what's the error for security reasons.
+            model.addAttribute("health", "Status Error");
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
     }
