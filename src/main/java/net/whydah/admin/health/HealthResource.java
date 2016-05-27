97343bdfaae1f09d933f84d6ba1e94c2f76e0bb2
totto <totto@totto.org>
2016-05-27 09:49:41 +0200

    sdk 2.2-alpha-2 with updated health

--- src/main/java/net/whydah/admin/health/HealthResource.java
+++ src/main/java/net/whydah/admin/health/HealthResource.java
@@ -1,5 +1,7 @@
 package net.whydah.admin.health;
 
+import net.whydah.sso.authentication.whydah.clients.WhydahServiceClient;
+import net.whydah.sso.util.WhydahUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -10,23 +12,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.Response;
 
-/**
- * Endpoint for health check, copied from UIB.
- */
 @Controller
 public class HealthResource {
+    private final WhydahServiceClient tokenServiceClient;
+
     private static final Logger log = LoggerFactory.getLogger(HealthResource.class);
 
-    public HealthResource() {
+    public HealthResource() throws Exception {
+        this.tokenServiceClient = new WhydahServiceClient();
+
     }
 
     @RequestMapping("/health")
     public Response isHealthy(HttpServletRequest request, HttpServletResponse response, Model model) {
         boolean ok = true;
-        log.trace("isHealthy={}", ok);
+        String statusText = WhydahUtil.getPrintableStatus(tokenServiceClient.getWAS());
+
+        log.trace("isHealthy={}, status: {}", ok, statusText);
         if (ok) {
             model.addAttribute("health", "Status OK!");
-            return Response.ok("Status OK!").build();
+            return Response.ok("Status OK!\n" + statusText).build();
         } else {
             //Intentionally not returning anything the client can use to determine what's the error for security reasons.
             model.addAttribute("health", "Status Error");
@@ -34,3 +39,4 @@ public class HealthResource {
         }
     }
 }
+
